dd6bc963927f0949f4e0a67fcf49723ea3e510b2
Timm <timmcliu@gmail.com>
2019-07-12 00:06:28 -0700

    firebase setup

---
+++ src/firebase.js
@@ -0,0 +1,12 @@
+const firebase = require("firebase/app")
+require("firebase/database")
+
+const config = {
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY || "AIzaSyCaQuffwJ_b8MOKQO8qvb3Wp-9LO1TYjIg",
+  databaseURL: process.env.REACT_APP_FIREBASE_DATABASE_URL || "https://load-board-c4eff.firebaseio.com/",
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID || "load-board-c4eff"
+}
+const app = firebase.initializeApp(config)
+const db = firebase.database(app)
+
+export default db
