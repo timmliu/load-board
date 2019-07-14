d0ce07e2186035d36d7dd171d51a04726d343e7c
Timm <timmcliu@gmail.com>
2019-07-13 21:58:40 -0700

    header

---
+++ src/components/Header/Header.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import styles from './Header.module.css'
+
+const Header = () => {
+  return (
+    <div className={styles.header}>
+      refresh
+    </div>
+  )
+}
+
+export default Header
