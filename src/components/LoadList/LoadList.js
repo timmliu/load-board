63e654b0d8dae18a56fb1948f456896d911f8a39
Timm <timmcliu@gmail.com>
2019-07-16 00:36:27 -0700

    handle basic loading and error states

--- src/components/LoadList/LoadList.js
+++ src/components/LoadList/LoadList.js
@@ -1,4 +1,4 @@
-import React, { useContext, useEffect } from 'react'
+import React, { useContext, useEffect, useState } from 'react'
 import { db, useListVals } from '../../firebase'
 import styles from './LoadList.module.css'
 import Load from '../Load/Load'
@@ -6,20 +6,31 @@ import LoadDetailed from '../LoadDetailed/LoadDetailed'
 import { DisplayContext } from '../../App'
 
 const LoadList = () => {
-  const [values] = useListVals(db.ref('loads'))
+  const [isLoading, setIsLoading] = useState(true)
+  const [values, loading, error] = useListVals(db.ref('loads'))
   const { displayAvailableOnly, selectedLoad, setSelectedLoad } = useContext(DisplayContext)
-  const loads = displayAvailableOnly ? values.filter(v => v.status === 'available') : values
+  const loads = [].concat(displayAvailableOnly ? values.filter(v => v.status === 'available') : values)
 
   useEffect(() => {
     if (selectedLoad) setSelectedLoad(loads.find(load => load.id === selectedLoad.id))
   }, [loads, selectedLoad, setSelectedLoad])
 
+  useEffect(() => {
+    setIsLoading(loading)
+  }, [loading])
+
+  useEffect(() => {
+    if (error) alert('Something went wrong, please try again later.')
+  }, [error])
+
   return (
     <div className={ styles.loads }>
         {
-          loads.map(load => {
-            return <Load key={load && load.id} {...load} />
-          })
+          isLoading
+            ? <div>Loading…</div>
+            : loads.length > 0
+                ? loads.map(load => <Load key={load && load.id} {...load} />)
+                : <div>No loads available.</div>
         }
         { selectedLoad && <LoadDetailed /> }
     </div>
